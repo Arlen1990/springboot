8515d38e83232ea1b67ec1527abeae70a69f017e
arlenan1990 <arlenan1990@outlook.com>
2019-07-22 10:17:01 +0800

    提交配置文件demo

---
+++ springboot-mybatis-properties/src/main/java/com/spider/entity/User.java
@@ -0,0 +1,32 @@
+package com.spider.entity;
+
+public class User {
+	public User(){
+		
+	}
+	public User(String username,String password){
+		this.username=username;
+		this.password=password;
+	}
+	private String username;
+	private String password;
+	private String id;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
